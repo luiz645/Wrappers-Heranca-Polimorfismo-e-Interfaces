a4a70b81e8a6816949e6c311b38c5b6eca45baca
luiz felipe <luizfelipebatistadesouzab@gmail.com>
2026-05-26 09:52:07 -0400

    entenderdo os comando usando o Integer e int

---
+++ pasta2/index2.java
@@ -0,0 +1,15 @@
+package pasta2;
+
+public class index2 {
+    public static void main(String[] args) {
+        /*
+        Declare Integer x = null. Tente atribuir x a uma variavel int. O que acontece ao rodar? Explique.
+        */
+
+        Integer x = null;
+        int v = x;
+
+        System.out.println("escreva o valor de " + v);
+        
+    }
+}
